a4d1e36569815e3e511081523e7f92bde521d5c9
good-luck-4616 <zmeo2612@gmail.com>
2026-03-05 04:27:52 +0700

    fix: use ShouldBroadcastNow for chat events - bypass queue for realtime

--- app/Events/UserTyping.php
+++ app/Events/UserTyping.php
@@ -5,11 +5,11 @@ namespace App\Events;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class UserTyping implements ShouldBroadcast
+class UserTyping implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
